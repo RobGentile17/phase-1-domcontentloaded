bd8ba855117e52b1a1d3d92e879a6a00be3b073b
Rob <robert.gentile@powerhrg.com>
2021-05-31 15:28:52 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener( "DOMContentLoaded", () => 
+    updateTheDOM()
+  )
+  
+  updateTheDOM = () =>
+    document.getElementById( "text" )
+      .textContent = "This is really cool!"
+  
+
